fef24980860b6027587c39e687d68fe5fbe23a8b
anita-jamgbadi <jamgbanita@gmail.com>
2022-12-07 19:09:30 +0000

    task 2

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,7 +1,6 @@
 #/usr/bin/python3
 def uniq_add(my_list=[]):
-    if my_list:
-        count = 0
-        for item in set(my_list):
-            count += item
+    count = 0
+    for item in set(my_list):
+        count += item
     return count
