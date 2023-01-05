9e775c443668d8a1941d567358c89a0ab0992528
anita-jamgbadi <jamgbanita@gmail.com>
2023-01-05 15:07:35 +0000

    tasks 0 and 1

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,5 +1,10 @@
 #!/usr/bin/python3
+""" EMpty class : Square """
+
+
 class Square:
     """Empty class called square
     """
+
+
     pass
--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,17 @@
 #!/usr/bin/python3
+"""Create a class Square
+"""
+
+
 class Square :
     """ Instantiated class of Square
     """
+
 
     def __init__(self, size):
         """Initializes a square
         size: size of square defined
         """
+
+
         self.__size = size
