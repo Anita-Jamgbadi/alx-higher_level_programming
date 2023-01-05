4e3a9dde974d3e564f07d1d0e5ffd56edf9e2b7b
anita-jamgbadi <jamgbanita@gmail.com>
2023-01-05 15:05:08 +0000

    tasks 0 and 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+class Square :
+    """ Instantiated class of Square
+    """
+
+    def __init__(self, size):
+        """Initializes a square
+        size: size of square defined
+        """
+        self.__size = size
