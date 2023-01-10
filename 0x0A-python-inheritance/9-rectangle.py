4922f9b86d0d6fb1ffc1cd1c2b7a386f172121e7
anita-jamgbadi <jamgbanita@gmail.com>
2023-01-10 03:26:45 +0000

    task 9

---
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+""" This module contains a class called base geometry """
+
+
+class BaseGeometry():
+    """ Base geometry class """
+
+    def area(self):
+        """ Calculates area """
+        raise Exception('area() is not implemented')
+
+    def integer_validator(self, name, value):
+        if not isinstance(value, int):
+            raise TypeError('{} must be an integer'.format(name))
+        if not isinstance(name, str):
+            raise TypeError('{} must be an string'.format(name))
+        if value <= 0:
+            raise ValueError('{} must be greater than 0'.format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ Rectangle inherits fron BaseGeometry """
+
+    def __init__(self, width, height):
+        """ Initialize a rectangle """
+
+        self.integer_validator('width', width)
+        self.__width = width
+        self.integer_validator('height', height)
+        self.__height = height
+
+    def area(self):
+        """ Return the area of the rect """
+
+        return (self.__width * self.__height)
+
+    def __str__(self):
+        """ prints a rect to stdout """
+
+        return ('[Rectangle] {}/{}'.format(self.__width, self.__height))
