55a1720d421e3c91ecc0a4eb8321375f4641799d
anita-jamgbadi <jamgbanita@gmail.com>
2023-01-10 02:46:23 +0000

    task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Checks if an obj is an instance of a class """
+
+
+def is_kind_of_class(obj, a_class):
+    """ Checks if obj is an instance of a_class """
+
+    if isinstance(obj, a_class):
+        return True
+    return False
