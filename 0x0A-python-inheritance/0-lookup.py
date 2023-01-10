06817ce41141024147b65f7d14ddd3c73914bdbb
anita-jamgbadi <jamgbanita@gmail.com>
2023-01-10 02:23:54 +0000

    task o

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+""" This module returns all available attributes and methods of an object """
+
+
+def lookup(obj):
+    """ Lookup and return a list of the available atts and methods for 'obj' """
+
+    return (obj.__dict__)
