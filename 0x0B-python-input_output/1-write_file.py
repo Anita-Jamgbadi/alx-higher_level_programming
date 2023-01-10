e100c6f91571a5363ca22046d5ff2565367b1ca4
anita-jamgbadi <jamgbanita@gmail.com>
2023-01-10 20:53:55 +0000

    task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -6,5 +6,5 @@ and returns the no of characters written """
 def write_file(filename="", text=""):
     """ write text into filename and return the no of chars written """
 
-    with open(filename, 'r+', encoding='utf-8') as f:
+    with open(filename, 'w', encoding='utf-8') as f:
         return f.write(text)
