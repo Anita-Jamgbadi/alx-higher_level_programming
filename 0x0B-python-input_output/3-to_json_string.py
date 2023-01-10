b9699eb7a327bcbe4af31844803a360117f8871e
anita-jamgbadi <jamgbanita@gmail.com>
2023-01-10 21:07:32 +0000

    task 3

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" This module uses to_json_string to return the JSON
+representation of an object """
+import json
+
+
+def to_json_string(my_obj):
+    """ Prints the json rep of a string """
+
+    return json.dumps(my_obj)
