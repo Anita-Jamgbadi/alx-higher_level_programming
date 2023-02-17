5cee8ecc55c391340fff0705169b70b26db65059
anita-jamgbadi <jamgbanita@gmail.com>
2023-02-17 00:25:49 +0000

    task 101

--- 0x0D-SQL_introduction/101-avg_temperatures.sql
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -1,4 +1,3 @@
 --- imports my database into another downloaded database
 
-SELECT city, AVG(value) AS avg_temp FROM temperatures GROUP BY city ORDER BY avg_temp DESC;
-
+SELECT city, AVG(value) AS avg_temp FROM temperatures ORDER BY avg_temp DESC;
