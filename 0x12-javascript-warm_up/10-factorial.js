84e7e7102c3153e7753c25cffa38eef0ec5ddf83
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-13 21:32:48 +0000

    task 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2]);
+
+if (!num) {
+  console.log('1');
+} else {
+  console.log(factorial(num));
+}
+
+function factorial (a) {
+  if (a === 0 || a === 1) {
+    return (1);
+  }
+  return a * factorial(a - 1);
+}
