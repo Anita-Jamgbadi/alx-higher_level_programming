a6f19cfbfe7f7d9cbc87eeb52656d671b2ff2aba
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-14 23:48:47 +0000

    2,3

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -2,10 +2,8 @@
 
 class Rectangle {
   constructor (w, h) {
-    if (typeof w === 'number' && w > 0) {
+    if ((typeof w === 'number' && w > 0) && (typeof h === 'number' && h > 0)) {
       this.width = w;
-    }
-    if (typeof h === 'number' && h > 0) {
       this.height = h;
     }
   }
