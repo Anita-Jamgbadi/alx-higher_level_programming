66323bd01a978f3a86168cacaa91133cba3d81c7
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-15 00:29:44 +0000

    100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,9 +2,7 @@
 
 const list = require('./100-data').list;
 
-const newList = list.map((value, index) => {
-  return value * index;
-});
+const newList = list.map((value, index) => value * index);
 
-console.log('Initial list:', list);
-console.log('New list:', newList);
+console.log(list);
+console.log(newList);
