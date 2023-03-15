f3dfc58d8d4a24de36a32c6b5045f95576e7ca0d
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-15 00:41:28 +0000

    101

---
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+
+const { dict } = require('./101-data.js');
+const newDict = {};
+for (const [userId, occurrences] of Object.entries(dict)) {
+  if (!newDict[occurrences]) {
+    newDict[occurrences] = [];
+  }
+  newDict[occurrences].push(userId);
+}
+
+console.log(newDict);
