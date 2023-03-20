9850a07ec052484991fb30fbc2a6eef40a9c768f
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-20 09:47:16 +0000

    6

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -10,6 +10,10 @@ from model_state import Base, State
 from sqlalchemy import (create_engine)
 
 if __name__ == '__main__':
+    '''
+    Defines a state cladd and a base class to work
+    with MySQLAlchemy ORM
+    '''
     engine = create_engine(
             'mysql+mysqldb://{}:{}@localhost/{}'
             .format(sys.argv[1], sys.argv[2], sys.argv[3]),
