435561a9c8f375790332c8d36b43c3f80d45ca6d
anita-jamgbadi <jamgbanita@gmail.com>
2023-03-20 10:39:59 +0000

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""
+Lists all sates from a db using sqlalchemy
+"""
+
+from model_state import Base, State
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == '__main__':
+    """
+    Fetches the states from a given db
+    """
+
+    url = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+            argv[1], argv[2], argv[3])
+    engine = create_engine(url)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).order_by(State.id):
+        print('{0}: {1}'.format(instance.id, instance.name))
